266544658374a13de3389af3da45d05de59f9f31
lucas <lucasriibeiro23@gmail.com>
2023-09-08 12:02:52 -0300

    requisito 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,55 @@
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../types';
+
 function Table() {
-  return <div>Table</div>;
+  const data = useSelector((globalState: ReduxState) => globalState);
+  const { wallet: { expenses } } = data;
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses && expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{Number(expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+              <td>
+                {(Number(expense.value) * expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button>Editar</button>
+                <button
+                  data-testid="delete-btn"
+                  id={ expense.id.toString() }
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
